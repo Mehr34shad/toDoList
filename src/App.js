47d9bf28c67c5c0994997c6a80aad15f992cd4d6
Mehrshad Moradshan <mehr.m6534@gmail.com>
2022-12-26 23:31:55 +0330

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './App.css';
-import TodoForm from './components/TodoForm';
+
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="todo-app">
-      <h1>Todo app</h1>
-      <TodoForm />
+      <TodoList />
     </div>
   );
 }
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -3,23 +3,25 @@ import React, { useState } from 'react';
 function TodoForm(props) {
   const [input, setInput] = useState('');
 
-  const handleChange = () => {
+  const handleChange = (e) => {
     setInput(e.target.value);
   };
 
   const handleSubmit = (e) => {
+    
     e.preventDefault();
 
     props.onSubmit({
       id: Math.floor(Math.random() * 10000),
       text: input,
     });
+    setInput('');
   };
   return (
     <form className="todo-form" onSubmit={handleSubmit}>
       <input
         type="text"
-        placeholder="کار جدید"
+        placeholder="Add a todo"
         value={input}
         name="text"
         className="todo-input"
