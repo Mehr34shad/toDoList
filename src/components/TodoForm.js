faa0d39a69bdff904fa5ad802fd0ebb7d9ca36a8
Mehrshad Moradshan <mehr.m6534@gmail.com>
2022-12-26 19:17:47 +0330

    new add

---
+++ src/components/TodoForm.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+function TodoForm() {
+    const [input, setInput] = useState('')
+    return (
+        <div>TodoForm</div>
+    )
+}
+
+export default TodoForm
