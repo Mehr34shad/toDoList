58f8a8addc2de9c5e4a04783a1a68db2fae2d8b9
Mehrshad Moradshan <mehr.m6534@gmail.com>
2022-12-26 18:55:28 +0330

    new app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from "react";
-import { render } from "react-dom";
-import App from "./containers/App";
-import GlobalState from "./containers/GlobalState";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-render(
-  <GlobalState>
-      <App />
-  </GlobalState>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
